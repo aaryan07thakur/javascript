ec5c6cd88b732108ef8a5dfc1d5b496f33704d5b
Aaryan Thakur <princeaaryan06@gmail.com>
2025-06-24 12:16:20 +0545

    update with practice questions

--- variable.js
+++ variable.js
@@ -25,64 +25,64 @@
 
 
 
-const n=100;
-// n=50   // output =error
-console.log(n)
+// const n=100;
+// // n=50   // output =error
+// console.log(n)
 
 
-// datatypes
+// // datatypes
 
 
-let age=20;
-typeof age
-// console.log(age)
+// let age=20;
+// typeof age
+// // console.log(age)
 
-let price=11.03
-let name="dipesh"
-
-
-let x;
-
-//Non primitive data types
-//object
-const students={
-    fullName:"Dipesh",
-    age:24,
-    class:"python",
-    gpa:7.9
-
-}
-
-// console.log(students.fullName)
-console.log(students["class"]);
+// let price=11.03
+// let name="dipesh"
 
 
+// let x;
 
+// //Non primitive data types
+// //object
+// const students={
+//     fullName:"Dipesh",
+//     age:24,
+//     class:"python",
+//     gpa:7.9
 
-// create a const object called "product " to store information shown in the pricture.
-const product={
-    Name: "Parker Jotter Standard CT Ball Pen",
-    rating:"4.5",
-    offer:5,
-    price: 270,
-};
+// }
 
-// console.log(product);
+// // console.log(students.fullName)
+// console.log(students["class"]);
 
 
 
-const profile={
-    Name:"Dipesh Thakur",
-    about:"codder boy",
-    email:"princeaaryan06@gmail.com",
-    isfollow:false,
-    followers:"569K",
-    following:4,
-    posts:195,
 
-};
-console.log(profile);
-console.log(typeof profile["isfollow"]);
+// // create a const object called "product " to store information shown in the pricture.
+// const product={
+//     Name: "Parker Jotter Standard CT Ball Pen",
+//     rating:"4.5",
+//     offer:5,
+//     price: 270,
+// };
+
+// // console.log(product);
+
+
+
+// const profile={
+//     Name:"Dipesh Thakur",
+//     about:"codder boy",
+//     email:"princeaaryan06@gmail.com",
+//     isfollow:false,
+//     followers:"569K",
+//     following:4,
+//     posts:195,
+
+// };
+// console.log(profile);
+// console.log(typeof profile["isfollow"]);
 
 
 
@@ -97,8 +97,56 @@ console.log(typeof profile["isfollow"]);
 //practice 
 // Declear a variable name and assign your full name to it. Then print it using console.log()
 
-const abc="Dipesh Thakur"
-console.log(abc)
+// const abc="Dipesh Thakur"
+// console.log(abc)
+
+
+//Create three variables: age, isStudent, and height. 
+//Assign appropriate values to them and print their data types using typeof.
+
+// const  age=27;
+// typeof age
+// console.log(typeof age)
+// const isStudent=true;
+// console.log(typeof isStudent)
+// const height="6 feet";
+// console.log(typeof  height)
+
+
+
+
+
+
+// What will be the output of the following code? Why?
+// var a = "5";
+// var b = 5;
+// console.log(a == b);  // (==) is the loose equality operator,so it converts the data types before comparing output=true
+
+// console.log(a === b); // (===) is the strict equality operator, it does not convert types so output=false
+
+
+// Write code to swap the values of two variables x = 10 and y = 20.
+
+// let x=10;
+// let y=20;
+
+// //sap using temp variable
+// let z=y;
+// y=x;
+// x=z;
+
+// console.log("x=",x);
+// console.log("y=",y);
+
+
+
+const name = "Dipesh Thakur";
+const age = 21;
+const isStudent = true;
+console.log("My name is " + name + ", I am " + age + " years old, and I am a student: " + isStudent);
+
+
+
 
 
 
