914f646b36bd58ac3c8f3b8fc69e30502c1525ef
Aaryan Thakur <princeaaryan06@gmail.com>
2025-06-26 17:36:31 +0545

    update with string learning in js

---
+++ string.js
@@ -0,0 +1,66 @@
+// let str="Thankyou god";
+// let str2='Thankyou God';
+
+// Escape character
+// console.log("Dipesh\n Thakur")
+// console.log("Dipesh\tThakur")
+
+
+// let str="dipesh Thakur";
+// str=str.toUpperCase();
+// console.log(str)
+
+
+
+// // trim method:
+// let str="   dipeshThakur     ";
+// console.log(str.trim());
+
+
+
+// let str="Thankyou GOd";
+// // str=str.slice(3,7)
+// str=str.slice(0,5)  // last index laie include gardai n 
+// console.log(str)
+
+
+
+
+//concinate method of js
+// let str1="Thankyou";
+// let str2="  God";
+// console.log(str1.concat(str2));
+
+
+
+//str.replace method
+
+// let str="Hello";
+// // Hello laie replace gare r Thankyou god print gar x 
+// // console.log((str.replace("Hello","Thank you God")))
+// console.log(str.replaceAll("ll","lp"));
+
+
+
+//str.CharAt(index):
+// let str="Thank you God"
+// console.log(str.charAt(2));
+
+
+// let str="dipesh Thakur";
+// str=str.replace("i","S")
+// console.log(str)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
