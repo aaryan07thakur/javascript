5ddb4894b9be763d3669640de1fb2c0bed3f2f67
Aaryan Thakur <princeaaryan06@gmail.com>
2025-06-27 15:01:22 +0545

    update_with learning array

---
+++ array.js
@@ -0,0 +1,70 @@
+// creating array
+// let marks=[1,2,3,4,5,66,77]
+// let info=["dipesh thakur",88,"kathmandu"]
+
+// console.log(marks)
+// console.log(Array.isArray(marks))
+// console.log(marks.length)
+// console.log(info)
+// console.log(Array.isArray(info))
+
+
+
+
+// looping over array
+//for loop
+// let names=["dipesh", "rupesh","ram","mukehs","sunaj"]
+// for (let i=0; i<names.length; i++){
+//     console.log(names[i]);
+
+// }
+
+
+//for of loop
+//  let names=["dipesh", "rupesh","ram","mukehsh","sujan","aaryan"]
+//  for (let el of names){
+//     console.log(el);
+//  }
+
+
+//example 2:
+let names=["dipesh", "rupesh","ram","mukehsh","sujan","aaryan"]
+ for (let el of names){
+    console.log(el.toUpperCase());
+ }
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
