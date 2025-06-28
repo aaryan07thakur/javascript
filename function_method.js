f135814b4a96af0c3d99ecd29240cefbb36896f0
Aaryan Thakur <princeaaryan06@gmail.com>
2025-06-28 18:35:26 +0545

    completeed chapter

--- function_method.js
+++ function_method.js
@@ -161,13 +161,91 @@ check_consunant("A quick Brown Fox jumps over the crezy dog")
 
 
 
+// For each loop: array ko every element ko laigie khe kam garau nu x vane use hun x 
+
+// let arr=[1,2,3,4,5,6];
+// arr.forEach(function printval(val){
+//     console.log(val);
+     
+// });
+
+
+//arrow function
+// let arry=[1,2,3,4,5,6,7,8]
+// let arry=["dipesh","sushil","mukesh"]  // index and arry itself pani mil x parameter print gar x 
+// arry.forEach((val,idx,arry)=>{
+//     console.log(val)
+//     console.log(val.toUpperCase(), idx,arry)  ///upper case ma print gar x 
+// });
+
+
+
+
+
+// for a given array of numbers, Print the square of each value using the forEach loop.
+
+// let aarr_num=[2,3,4,5,6]
+// aarr_num.forEach((aarr_num)=>{
+//     // console.log(aarr_num*aarr_num);
+//     console.log(aarr_num**2);   // using exponantional operator
+// });
+
+
+// next method
+// let num=[4,5,6,7,8]
+// let calculatesqr=(num)=>{
+//     console.log(num**2);
+// }
+// num.forEach(calculatesqr)
+
+
+
+
+
+// map method:
+
+// let num=[4,5,6,7,8]
+// num.map((val)=>{
+//     console.log(val);
+//     console.log(val**2);
+    
+// });
+
+
+// let newarr=num.map((val)=>{
+//     // return val;
+//     return val**2;
+// });
+// console.log(newarr)
 
 
 
 
 
 
+// filter method
 
+// let abc =[1,2,3,4,5,6,7,8,9,22,33,44,55,66,75,44,23,];
+// let even_arr=abc.filter((val)=>{
+//     // return val%2===0;
+//     return val>3;  // larger than 3 
+
+// })
+// console.log("the even numbber is=",even_arr)
+
+
+
+
+//reduce method
+
+// let arr=[2,3,4,5,6,7]
+// const output=arr.reduce((prev, curr)=>{
+//     // return prev>curr? prev:curr   // largest number laie return gar x
+//     return prev<curr? prev:curr // small number return gara x
+
+
+// });
+// console.log(output);
 
 
 
