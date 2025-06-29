e4766895eb5d5cf42eba25b31ac2796976884327
Aaryan Thakur <princeaaryan06@gmail.com>
2025-06-29 18:27:55 +0545

    DOM topic and practice questions

--- DOM_chapter6/scripts.js
+++ DOM_chapter6/scripts.js
@@ -51,17 +51,57 @@ console.log("Thank You god");
 
 
 // id laie queryselector bata access garna ko lagie #idname lekhnu par  x
-let id=document.querySelector("#heading")  // this gives first element only 
-console.log(id);
-// console.dir(id);
+// let id=document.querySelector("#heading")  // this gives first element only 
+// console.log(id);
+// // console.dir(id);
 
-let id2=document.querySelectorAll("#heading")  // this return all  element in nodelist 
-console.log(id2);
-// console.dir(id2);
-
-
+// let id2=document.querySelectorAll("#heading")  // this return all  element in nodelist 
+// console.log(id2);
+// // console.dir(id2);
 
 
+
+// console.log(document.body.firstChild);
+
+
+// let div=document.querySelector("div");
+// console.log(div);
+// console.log(div.innerText)  // innerText vane ko div vitra ko sabai element
+
+
+
+
+// let h2=document.querySelector("h2");
+// console.log(h2.innerText);
+
+// h2.innerText=h2.innerText+ " from youtube"// concanitate 
+// console.log(h2.innerText);
+
+
+// let divs=document.querySelectorAll(".box");
+// console.log(divs);
+
+// divs[0].innerText="new unique text 0" // changing text
+// console.log(divs[0])
+
+// divs[1].innerText="new unique text1" // changing text
+// console.log(divs[1])
+
+// divs[2].innerText="new unique text2" // changing text
+// console.log(divs[2])
+
+
+
+
+//using loops
+let divs=document.querySelectorAll(".box");
+let idx=1;
+for (div of divs){
+    // console.log(div.innerText);
+    div.innerText=`new unique value ${idx}`;
+    idx++;
+}
+console.log(div)
 
 
 
