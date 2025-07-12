cd29dffc295a195bac1975bba4df68d3f1c8f595
Aaryan Thakur <princeaaryan06@gmail.com>
2025-07-12 16:21:14 +0545

    update with tracking click status

--- counter_project/app.js
+++ counter_project/app.js
@@ -1,16 +1,31 @@
 let count=0;
+let inccounts=0;
+let deccounts=0;
 
 const countDisplay=document.getElementById("count");
 const incrementBtn=document.getElementById("increment-btn");
 const decrementBtn=document.getElementById("decrement-btn");
+const incDisplay=document.getElementById("inc-count");
+const decDisplay=document.getElementById("dec-count");
 
-incrementBtn.addEventListener("click",()=>{
+
+const inccount=incrementBtn.addEventListener("click",()=>{
     count++;
+    inccounts++;
+    incDisplay.textContent=inccounts;
     countDisplay.textContent=count;
 });
 
-decrementBtn.addEventListener("click",()=>{
+const deccount=decrementBtn.addEventListener("click",()=>{
     count--;
+    deccounts++;
+    decDisplay.textContent=deccounts;
     countDisplay.textContent=count;
 });
+
+function update(){
+    count.textContent=count;
+    inccount=textContent=incrementBtn;
+    deccount=textContent=decrementBtn;
+}
 
