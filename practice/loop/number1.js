e634c0b0f5274863baa20b43cd9be948ac4e62e0
Aaryan Thakur <princeaaryan06@gmail.com>
2025-07-23 00:20:17 +0545

    second question

--- practice/loop/number1.js
+++ practice/loop/number1.js
@@ -1,5 +1,5 @@
 // write a javascript program that displays the largest integer among two integers.
-
+/*
 let fnum= parseInt(prompt("Enter the first number"));
 let snum=parseInt(prompt("Enter the second num"));
 
@@ -11,3 +11,29 @@ if (fnum> snum){
     console.log(`${fnum} and ${snum} are equal`);
 
 }
+
+*/
+
+
+
+
+// Write a JavaScript function that determines the larger of two integers using
+//  bitwise operators only.
+
+function getlargerBitwise(a,b){
+    //calculating diff
+    let diff=a-b;
+
+    //get the sign bit (0 if diff is positive, 1 if negative)
+    let k=(diff>>31)&1;
+
+    // if k ==0 then a is larger; if k ==1 then b is larger
+    return a-k*diff;
+}
+
+console.log(getlargerBitwise(10,20));
+
+
+
+
+
