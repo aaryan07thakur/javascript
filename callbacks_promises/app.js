e383d3d74c8f1d189cd0a89ee8b53c6a1dc1202f
Aaryan Thakur <princeaaryan06@gmail.com>
2025-07-25 23:53:19 +0545

    promise and promise chaining

--- callbacks_promises/app.js
+++ callbacks_promises/app.js
@@ -45,24 +45,105 @@
 // ======================================================
 // next example
 
-function getData(dataId,getNextData){
-    setTimeout(() => {
-        console.log("data", dataId);
-      if (getNextData){
-        getNextData();
-      }  
+// function getData(dataId,getNextData){
+//     setTimeout(() => {
+//         console.log("data", dataId);
+//       if (getNextData){
+//         getNextData();
+//       }  
         
-    }, 2000);// 2 sec pachhi data laei print gar x 
-}
+//     }, 2000);// 2 sec pachhi data laei print gar x 
+// }
 // 2 2 sec ko gap ma sabai data1,data2,data3 and data4 print gar x 
 // this is a callbackhell
-getData(1,()=>{
-    getData(2,()=>{
-        getData(3,()=>{
-            getData(4);
-        });
+// getData(1,()=>{
+//     getData(2,()=>{
+//         getData(3,()=>{
+//             getData(4);
+//         });
+//     });
+// });
+
+
+
+
+// =====================================================================================================
+// Promises
+// let promise=new Promise((resolve, reject)=>{
+//     console.log("I am a promice");
+//     // resolve("Success");
+//     reject("some error");
+// });
+
+
+// let promise=getData(123);
+// function getData(dataid,getNextData){
+//     return new Promise((resolve, reject)=>{
+//         setTimeout(()=>{
+//             console.log("data: ", dataid);
+//             resolve("Success");
+//             console.log(promise);
+//             if(getNextData){
+//                 getNextData();
+//             }
+//         },5000);
+//     });
+
+//     }
+
+
+// function fakeApiCall() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       let success = true;
+//       if (success) {
+//         resolve(" Data received from server");
+//       } else {
+//         reject(" Failed to fetch data");
+//       }
+//     }, 2000); // 2 second wait
+//   });
+// }
+
+// fakeApiCall()
+//   .then((data) => console.log(data))
+//   .catch((err) => console.error(err))
+//   .finally(() => console.log("API call finished"));
+
+
+// ===============================================================================
+// chaning promises
+
+function asyncfun(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            console.log("some data1");
+            resolve("success");
+        },4000);
+    });
+}
+
+function asyncfun2(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            console.log("data2");
+            resolve("success");
+        },6000);
+    });
+}
+// promise chaining
+console.log("fetching data1..............");
+let p1=asyncfun();
+p1.then((res)=>{
+    console.log("result:",res);
+    console.log("fetching data2......");
+    let p2=asyncfun2();
+    p2.then((res)=>{
+        console.log("result:",res);
+
     });
 });
+
 
 
 
