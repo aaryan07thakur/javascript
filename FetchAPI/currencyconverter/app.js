aae85b4c5f8c5d955330657aada420a5923aaf65
Aaryan Thakur <princeaaryan06@gmail.com>
2025-07-28 00:24:48 +0545

    complete fetching api

--- FetchAPI/currencyconverter/app.js
+++ FetchAPI/currencyconverter/app.js
@@ -1,6 +1,12 @@
-const BASE_URL="https://2024-03-06.currency-api.pages.dev/v1/currencies/eur.json"
+// const BASE_URL="https://api.exchangerate-api.com/v4/latest/NPR";
+// const BASE_URL = "https://api.exchangerate-api.com/v4/latest/USD";
+
 
 const dropdowns= document.querySelectorAll(".dropdown select");
+const btn =document.querySelector("form button");
+const fromCurr=document.querySelector(".from select");
+const toCurr=document.querySelector(".to select");
+const msg=document.querySelector(".msg");
 
 
 for (let select of dropdowns){
@@ -10,14 +16,43 @@ for (let select of dropdowns){
     newOption.value=currCode;
     if (select.name==="from" && currCode=== "USD"){
         newOption.selected="selected";
-    }else if (select.name==="To" && currCode==="NPR"){
+    }else if (select.name==="to" && currCode==="NPR"){
         newOption.selected="selected";
     }
     select.append(newOption);
-
 }
+select.addEventListener("change", (evt) => {
+        updateFlag(evt.target);
+
+    });
 }
 
+const updateFlag =(element) => {
+    let currCode=element.value;
+    let countryCode=countryList[currCode];
+    let newSrc=`https://flagsapi.com/${countryCode}/flat/64.png`
+    let img=element.parentElement.querySelector("img");
+    img.src=newSrc;  
+};
+
+btn.addEventListener("click", async (evt)=>{
+    evt.preventDefault();// page refresh huna banda hunx  button click vayo vnae 
+    let amount=document.querySelector(".amount input");
+    let amtVal=amount.value;
+    if(amtVal==="" || amtVal<1){
+        amtVal=1;
+        amount.value="1";
+    }
+
+    let response= await fetch(`https://api.exchangerate-api.com/v4/latest/${fromCurr.value}`);
+    let data=await response.json();
+    let rate = data.rates[toCurr.value];
+    let finalAmount=amtVal*rate;
+    msg.innerText=`${amtVal} ${fromCurr.value}=${finalAmount} ${toCurr.value}`
+
+
+
+});
 
 
 
